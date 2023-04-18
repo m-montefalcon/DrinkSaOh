bf9d6de78eca93bc7b4bf4da081f5314aedb6410
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-04-19 00:15:54 +0800

    Backend inventory commit

--- code.php
+++ code.php
@@ -68,6 +68,44 @@ if(isset($_POST['edit_inventory']))
 //ADD
 
 
+// if(isset($_POST['add_inventory']))
+// {
+//     $sku_number = $_POST['sku_number'];
+//     $sku_qty = $_POST['sku_qty'];
+//     $redColor = [255, 0, 0];
+
+//     $barcode_data = $sku_number . "-" . $sku_qty;
+ 
+   
+//     $barcode = new \Picqer\Barcode\BarcodeGeneratorHTML();
+//     $barcode_img = $barcode->getBarcode($barcode_data, $barcode::TYPE_CODE_128);
+
+//     $postData = [
+//         'eId' => $_SESSION['verified_user_id'],
+//         'skuId' => $sku_number,
+//         'skuQtyId' => $sku_qty,
+//         'barcode' => $barcode_img,
+//         'currentDate' => date('Y-m-d'),
+//         'currentTime' => date('H:i:s')
+//     ];
+
+//     $ref_table = "inventory";
+//     $postRef_result = $database->getReference($ref_table)->push($postData);
+    
+//     if($postRef_result)
+//     {
+//         $_SESSION['status'] = "Added!";
+//         header('location: index.php');
+
+//     }else
+//     {
+//         $_SESSION['status'] = "Error!";
+//         header('location: index.php');
+//     }
+   
+
+// }
+
 if(isset($_POST['add_inventory']))
 {
     $sku_number = $_POST['sku_number'];
@@ -80,33 +118,59 @@ if(isset($_POST['add_inventory']))
     $barcode = new \Picqer\Barcode\BarcodeGeneratorHTML();
     $barcode_img = $barcode->getBarcode($barcode_data, $barcode::TYPE_CODE_128);
 
-    $postData = [
-        'eId' => $_SESSION['verified_user_id'],
-        'skuId' => $sku_number,
-        'skuQtyId' => $sku_qty,
-        'barcode' => $barcode_img,
-        'currentDate' => date('Y-m-d'),
-        'currentTime' => date('H:i:s')
-    ];
-
     $ref_table = "inventory";
-    $postRef_result = $database->getReference($ref_table)->push($postData);
+    $inventoryQuery = $database->getReference($ref_table)->orderByChild('skuId')->equalTo($sku_number)->getSnapshot();
     
-    if($postRef_result)
-    {
-        $_SESSION['status'] = "Added!";
-        header('location: index.php');
+    if($inventoryQuery->exists()) {
+        // SKU already exists, update quantity instead of creating a new entry
+        foreach($inventoryQuery->getValue() as $key => $inventory) {
+            $newQty = $inventory['skuQtyId'] + $sku_qty;
+            $updateData = [
+                'eId' => $_SESSION['verified_user_id'],
+                'skuQtyId' => $newQty,
+                'barcode' => $barcode_img,
+                'currentDate' => date('Y-m-d'),
+                'currentTime' => date('H:i:s')
+            ];
+            $ref_table = 'inventory/'.$key;
+            $updateInventory = $database->getReference($ref_table)->update($updateData);
 
-    }else
-    {
-        $_SESSION['status'] = "Error!";
-        header('location: index.php');
+            if($updateInventory)
+        {
+            $_SESSION['status'] = "Added!";
+            header('location: index.php');
+    
+        }else
+        {
+            $_SESSION['status'] = "Error!";
+            header('location: index.php');
+        }
+        }
+    } else {
+        // SKU does not exist, create a new entry
+        $postData = [
+            'eId' => $_SESSION['verified_user_id'],
+            'skuId' => $sku_number,
+            'skuQtyId' => $sku_qty,
+            'barcode' => $barcode_img,
+            'currentDate' => date('Y-m-d'),
+            'currentTime' => date('H:i:s')
+        ];
+    
+        $postRef_result = $database->getReference($ref_table)->push($postData);
+        
+        if($postRef_result)
+        {
+            $_SESSION['status'] = "Added!";
+            header('location: index.php');
+    
+        }else
+        {
+            $_SESSION['status'] = "Error!";
+            header('location: index.php');
+        }
     }
-   
-
 }
-
-
 
 
 
